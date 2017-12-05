c26cda6b813e7410f1f0be0e1b738e0b8beaf236
Ludvig Noring <ludno249@student.liu.se>
2017-12-05 13:02:56 +0100

    bu

---
+++ 2017/05.py
@@ -0,0 +1,17 @@
+with open('.05.input') as f:
+    jumps = [int(line) for line in f]
+
+i = 0
+c = 0
+try:
+    while True:
+        if jumps[i] >= 3:
+            bla = -1
+        else:
+            bla = 1
+        jumps[i] += bla
+        i = i + jumps[i] - bla
+        c += 1
+    
+except:
+    print(c)
