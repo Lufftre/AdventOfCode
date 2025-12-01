03a9e5a169e395105b23ea6432a810d96b64a605
Ludvig Noring <ludvig.noring@eyevinn.se>
2025-12-01 10:48:09 +0100

    01 refactor

--- 2025/01.c
+++ 2025/01.c
@@ -1,76 +1,49 @@
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STAR2 1
+static inline uint8_t normalize(int dial) { return ((dial % 100) + 100) % 100; }
 
+static uint16_t star1(FILE *f) {
+  uint16_t score = 0;
+  uint8_t dial = 50;
+  char buf[8];
 
-void star1(FILE* f) {
-    int s = 0;
-    int dial = 50;
-    char line[256];
-    while (fgets(line, sizeof(line), f)) {
-        char dir = line[0];
-        int d = atoi(line + 1);
-        d %= 100;
-        if (dir == 'L') {
-           dial -= d;
-           if (dial < 0) {
-               dial += 100;
-           }
-        } else {
-            dial += d;
-        }
-        dial %= 100;
-        if (dial == 0) {
-            s += 1;
-        }
-        printf("%c %d => %d\n", dir, d, dial);
-    }
-    printf("%d\n", s);
+  while (fgets(buf, sizeof(buf), f)) {
+    int delta = atoi(buf + 1) % 100;
+    dial = normalize(dial + (buf[0] == 'L' ? -delta : delta));
+    if (!dial)
+      score++;
+  }
+  return score;
 }
 
-void star2(FILE* f) {
-    printf("Star 2\n");
-    int s = 0;
-    int dial = 50;
-    char line[256];
-    while (fgets(line, sizeof(line), f)) {
-        char dir = line[0];
-        int v = 1;
-        if (dir == 'L') {
-            v = -1;
-        }
-        int d = atoi(line + 1);
-        for (int i = 0; i < d; i++) {
-            dial += v;
-            if (dial < 0) {
-                dial += 100;
-            } else if (dial > 99) {
-                dial -= 100;
-            }
+static uint16_t star2(FILE *f) {
+  uint16_t score = 0;
+  uint8_t dial = 50;
+  char buf[8];
 
-            if (dial == 0) {
-                s += 1;
-            }
-        }
+  while (fgets(buf, sizeof(buf), f)) {
+    int step = buf[0] == 'L' ? -1 : 1;
+    for (int i = atoi(buf + 1); i--;) {
+      dial = normalize(dial + step);
+      if (dial == 0)
+        score++;
     }
-    printf("%d\n", s);
+  }
+  return score;
 }
 
 int main() {
-    FILE *file = fopen("01.input", "r");
-    if (file == NULL) {
-        printf("Error: Could not open file 01.input\n");
-        return 1;
-    }
+  FILE *f;
+  uint16_t result;
+  f = fopen("01.input", "r");
+  result = star1(f);
+  fclose(f);
+  printf("star1: %u\n", result);
 
-    if(!STAR2)
-        star1(file);
-    else
-        star2(file);
-
-    fclose(file);
-    return 0;
+  f = fopen("01.input", "r");
+  result = star2(f);
+  printf("star2: %u\n", result);
+  fclose(f);
 }
