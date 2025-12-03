9b9940303b8929e17b633eb529975705793137f4
Ludvig Noring <ludvig.noring@eyevinn.se>
2025-12-03 10:02:48 +0100

    :golf:

--- 2025/03.py
+++ 2025/03.py
@@ -6,13 +6,11 @@ IN = """987654321111111
 
 def solve(line, n):
     res = ""
+    line = list(map(int, list(line)))
     for j in range(n, 0, -1):
-        for i in range(9, 0, -1):
-            idx = line[: len(line) - j + 1].find(str(i))
-            if idx != -1:
-                res += str(i)
-                line = line[idx + 1 :]
-                break
+        idx = line.index(max(line[: len(line) - (j - 1)]))
+        res += str(line[idx])
+        line = line[idx + 1 :]
 
     print(res)
     return int(res)
